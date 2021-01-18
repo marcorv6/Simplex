05cdd75c02e68f7d5188c665553aae9ba81d5cd7
mxrco <yosoymarco3@gmail.com>
2021-01-18 17:11:08 -0600

    parseArrays

--- model.js
+++ model.js
@@ -4,21 +4,32 @@ const app = new Vue({
     return {
       noVar: localStorage.getItem("noVar"),
       noRes: localStorage.getItem("noRes"),
-      objetivo: "",
+      objetivoIsMin: true,
       LD: [],
-      FO:[],
+      FO: [],
       operadores: [],
-      mat:[]
-
+      mat: [],
+      aux: []
     };
   },
-  methods: {},
+  methods: {
+    parseArrays() {
+      const max = this.noRes*this.noVar;
+      for(let i=0; i<max; i++){
+        if(i<this.noRes) this.LD[i] = Number(this.LD[i]);
+        if(i<this.noVar) this.FO[i] = Number(this.FO[i]);
+        this.aux[i] = Number(this.aux[i]);
+      }
+      console.log(this.LD, this.FO, this.aux);
+    }
+  },
   created() {
     for(let i=0; i<this.noRes; i++){
       this.mat[i] = new Array(Number(this.noVar))
     }
     this.LD = new Array(Number(this.noRes));
-    this.operadores = new Array(Number(this.noRes));
     this.FO = new Array(Number(this.noVar));
+    this.aux = new Array((Number(this.noVar)*Number(this.noRes)));
+    this.operadores = new Array(Number(this.noRes));
   }
 });
