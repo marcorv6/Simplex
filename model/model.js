2841ce048f73ea3c180a1dfb8cfaa7c306089856
mxrco <yosoymarco3@gmail.com>
2021-01-18 22:00:54 -0600

    acomodo de carpetas

--- model/model.js
+++ model/model.js
@@ -33,13 +33,10 @@ const app = new Vue({
       localStorage.setItem("mat", this.mat);
       localStorage.setItem("isMin", this.objetivoIsMin);
       localStorage.setItem("operadores", this.operadores);
-
     }
   },
   created() {
-    for(let i=0; i<this.noRes; i++){
-      this.mat[i] = new Array(Number(this.noVar))
-    }
+    for(let i=0; i<this.noRes; i++) this.mat[i] = new Array(Number(this.noVar))
     this.LD = new Array(Number(this.noRes));
     this.FO = new Array(Number(this.noVar));
     this.aux = new Array((Number(this.noVar)*Number(this.noRes)));
